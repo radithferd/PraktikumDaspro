c47b084d15389dbf7bc497f67d214b5f7a34142a
radithferd <radithferd@gmail.com>
2024-09-25 14:25:33 +0700

    Praktikum05

---
+++ Praktikum05/PemilihanHariDenganIf18.java
@@ -0,0 +1,17 @@
+package Praktikum05;
+
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int number;
+
+        System.out.println("Input number: ");
+        number = sc.nextInt();
+
+        if (number) {
+            
+        }
+
+}

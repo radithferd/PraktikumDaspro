9a2ed37929463a563d9aadf3e39975eb2f357bbc
radithferd <radithferd@gmail.com>
2024-10-10 13:42:47 +0700

    Praktikum06

--- Praktikum06/Pemilihan2Percobaan318.java
+++ Praktikum06/Pemilihan2Percobaan318.java
@@ -24,7 +24,17 @@ public class Pemilihan2Percobaan318 {
            pajak = 0.2;
         gajiBersih = (int) (penghasilan - (pajak * penghasilan));
         System.out.println("Penghasilan Bersih : " + gajiBersih);
-    } else 
-        System.out.println("Masukan Kategori Salah");
+    } else if (kategori.equalsIgnoreCase("pebisnis")){
+        if (penghasilan <= 2500000)
+            pajak = 0.15;
+        else if (penghasilan <= 3500000)
+            pajak = 0.2;
+        else
+            pajak = 0.25;
+        gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+        System.out.print("Penghasilan Bersih : " + gajiBersih);    
+    } else
+        System.out.print("Masukan Kategori Salah");
+        
     }
 }

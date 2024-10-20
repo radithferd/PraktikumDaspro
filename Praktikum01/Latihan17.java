5b4b47790d4c761e953cf182465d4030805b0a45
radithferd <radithferd@gmail.com>
2024-10-20 23:36:37 +0700

    Praktikum01

---
+++ Praktikum01/Latihan17.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan17
+ */
+public class Latihan17 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Radith Ferdian Hibawan");
+        System.out.print("\nSaya adalah Mahasiswa Baru Jurusan Teknologi Informasi-POLINEMA");
+        }
+    }

c27dbab33e5c96e4d1cfd796b58d0e608a7eb752
radithferd <radithferd@gmail.com>
2024-11-14 22:33:27 +0700

    Praktikum09

--- Praktikum09/ArrayRataNilai18.java
+++ Praktikum09/ArrayRataNilai18.java
@@ -3,22 +3,46 @@ import java.util.Scanner;
 public class ArrayRataNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jmlMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
+        int totalLulus = 0, totalTidakLulus = 0;
+        int jmlLulus = 0, jmlTidakLulus = 0;
+        int nilaiTertinggi = nilaiMhs[0];
+        int nilaiTerendah = nilaiMhs[0];
+
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jmlLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jmlTidakLulus++;
+            }
+
+            if (nilaiMhs[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiMhs[i];
+            }
+            if (nilaiMhs[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiMhs[i];
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        double rataLulus = (jmlLulus > 0) ? (totalLulus / jmlLulus) : 0;
+        double rataTidakLulus = (jmlTidakLulus > 0) ? (totalTidakLulus / jmlTidakLulus) : 0;
+
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        System.out.println("Nilai tertinggi = " + nilaiTertinggi);
+        System.out.println("Nilai terendah = " + nilaiTerendah);
 
     } 
 }
